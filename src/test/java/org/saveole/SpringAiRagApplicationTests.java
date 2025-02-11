74501b836108161bc469c2f81a5ea6c466473e12
saveole <17688703451@163.com>
2025-02-11 17:54:16 +0800

    init

---
+++ src/test/java/org/saveole/SpringAiRagApplicationTests.java
@@ -0,0 +1,13 @@
+package org.saveole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiRagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
